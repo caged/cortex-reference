7181bce57e42ab4f2d28461c556c95f850ca4b00
Justin Palmer <encytemedia@gmail.com>
2022-03-09 21:15:49 -0800

    highlight colors

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,20 @@
 <script>
   import amps from "./amps.json";
   import Brand from "./Brand.svelte";
+
+  const sorted = Object.entries(amps).sort((a, b) => {
+    return b[1].length - a[1].length;
+  });
 </script>
 
 <div class="h-full text-slate-700">
-  <div class="columns-4 p-2">
-    {#each Object.entries(amps) as [name, presets]}
+  <div class="columns-4 print:columns-3 p-5 print:p-0">
+    {#each sorted as [name, presets]}
       <Brand {name} {presets} />
     {/each}
+  </div>
+  <div class="absolute right-0 bottom-0 p-2 text-xs text-gray-400">
+    Quad Cortex Amps
   </div>
 </div>
 
--- src/Brand.svelte
+++ src/Brand.svelte
@@ -4,19 +4,44 @@
 
   const grouped = new Map(Array.from(presets).map((p) => [p.model, []]));
   presets.forEach((p) => grouped.get(p.model).push(p));
-  console.log(grouped);
+
+  const colors = {
+    Clean: "text-blue-500",
+    Lead: "text-green-500",
+    Crunch: "text-orange-500",
+    Bright: "text-yellow-500",
+    Normal: "text-gray-800",
+    Patch: "text-purple-500",
+  };
+
+  function colorName(name) {
+    const found = Object.entries(colors).find(([k, v]) => name.includes(k));
+    if (found) {
+      // const el = document.createElement("div");
+      // const txt = document.createTextNode(found[0]);
+      // el.appendChild(txt);
+      // return el;
+      return name.replace(
+        found[0],
+        `<span class="${found[1]}">${found[0]}</span>`
+      );
+    }
+    return name;
+  }
 </script>
 
-<div class="border rounded-lg mb-2 p-4 break-inside-avoid">
-  <h3 class="font-bold text-gray-900 mb-2 text-lg">{name}</h3>
-  <div class="space-y-5">
+<div class="p-2 break-after-all">
+  <h3 class="font-bold text-gray-900 mb-4 text-lg border-b-2 border-gray-700">
+    {name}
+  </h3>
+  <div class="space-y-2">
     {#each [...grouped] as [model, modpresets]}
       <div>
-        <h4 class="font-bold text-sm mb-2 text-gray-700">{model}</h4>
+        <h4 class="font-bold text-sm mb-1 text-gray-700">{model}</h4>
         <div class="space-y-2 text-xs">
           {#each modpresets as preset}
             <div class="text-gray-500">
-              &bull;&bull;&bull;&bull;&bull;&bull; {preset.name}
+              {@html colorName(preset.name)}
             </div>
           {/each}
         </div>
